71b5aa72d807f6b8f5fba93a9db85b29098631aa
leeyoshinari <lee_jc@outlook.com>
2020-07-25 08:07:15 +0800

    add function

--- handleException/handleException.py
+++ handleException/handleException.py
@@ -6,13 +6,14 @@
 import traceback
 
 
-def handle_exception(errors=(Exception, ), is_return=False, default_value=None):
+def handle_exception(errors=(Exception, ), is_return=False, is_return_error_msg=False, default_value=None):
 	"""
 	Handle exception, throw an exception, or return a value.
 	:param errors: Exception type
-	:param is_return: Whether to return 'default_value'. Default False, if exception, don't throw an exception, but return a value.
+	:param is_return: Whether to return a value. Default False, if exception, don't throw an exception, but return a value.
+	:param is_return_error_msg: Whether to return an error-msg. Default False, return a 'default_value'. If True, return an error-msg.
 	:param default_value: If 'is_return' is True, return 'default_value'.
-	:return: 'default_value'
+	:return: 'default_value' or error-msg
 	"""
 	def decorator(func):
 		def decorator1(*args, **kwargs):
@@ -21,7 +22,10 @@ def handle_exception(errors=(Exception, ), is_return=False, default_value=None):
 					return func(*args, **kwargs)
 				except errors:
 					print(traceback.format_exc())
-					return default_value
+					if is_return_error_msg:
+						return traceback.format_exc()
+					else:
+						return default_value
 			else:
 				try:
 					return func(*args, **kwargs)
